563656f7bcf84dbed0f13f402ec6b8d5c8060a29
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2021-03-05 17:07:50 +0100

    Commit 1

---
+++ Assets/Monster/Scripts/Monster.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Monster : MonoBehaviour
+{
+    public int scorePoint;
+
+    private ParticleSystem[] particleDead;
+    private AudioSource explosion;
+
+    private Text scoreText;
+    private score Score;
+
+    private void Awake()
+    {
+        explosion = gameObject.GetComponentsInChildren<AudioSource>()[0];
+        particleDead = GameObject.Find("Explosion 1").GetComponentsInChildren<ParticleSystem>();
+        Score = FindObjectOfType<score>();
+        StartCoroutine("changePosition");
+        scoreText = FindObjectOfType<Text>();
+        gameObject.transform.localScale = new Vector3(100,100,100);
+        
+    }
+    
+    private void dieMonster()
+    {
+        Debug.Log("matar");
+        
+        explosion.Play();
+        
+        foreach (ParticleSystem particle in particleDead)
+        {
+            if (particle != null)
+            {
+                particle.Play();
+            }
+        }
+        
+        foreach (ParticleSystem particle in particleDead)
+        {
+            if (particle != null)
+            {
+                particle.Stop();
+            }
+        }
+        
+        Destroy(gameObject);
+
+        score.Score += scorePoint;
+        
+        string scoreTextInformation = "Score " + score.Score;
+        scoreText.text = scoreTextInformation;
+        Debug.Log(scoreTextInformation);
+        
+        
+        Score.createMonster();
+    }
+    
+    IEnumerator changePosition()
+    {
+        while (gameObject.activeInHierarchy)
+        {
+            transform.position = Score.vectorAleatory();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    private void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(0)) {
+            dieMonster();
+        }
+    }
+}
---
+++ Assets/Monster/Scripts/score.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Random = UnityEngine.Random;
+
+public class score : MonoBehaviour
+{
+    public static int Score { get; set; }
+    public GameObject[] listMonster;
+    private static void initScore()
+    {
+        Score = 0;
+    }
+    private void Awake()
+    {
+        FindObjectOfType<Text>().text = "Score 0";
+        createMonster();
+    }
+
+    public void createMonster()
+    {
+        if (GameObject.FindGameObjectsWithTag("enemie").Length <= 1)
+        {
+            int listMonsterSize = listMonster.Length;
+            if (listMonsterSize >= 0)
+            {
+                int indexMonster = random(0, listMonsterSize);
+                Instantiate(listMonster[indexMonster], vectorAleatory(), Quaternion.identity);
+            }
+        }
+    }
+
+    private static float random(float startNumber, float endNumber)
+    {
+        return Random.Range(startNumber, endNumber);
+    }
+    
+    private static int random(int startNumber, int endNumber)
+    {
+        return Random.Range(startNumber, endNumber);
+    }
+
+    public Vector3 vectorAleatory()
+    {
+        float startNumber = 5.0f;
+        float endNumber = 10.0f;
+        float positionX = random(startNumber, endNumber);
+        float positionZ = random(startNumber, endNumber);
+        return new Vector3(positionX, 0, positionZ);
+    }
+}
